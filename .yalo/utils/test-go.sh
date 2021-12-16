ee14680a6a50e107eabb002e513c7a77871a857a
Warley Gabriel <gabriel@yalo.com>
2021-12-16 19:13:55 -0300

    feat: Change the way that we define the image's title that was sent by the end-user

--- .yalo/utils/test-go.sh
+++ .yalo/utils/test-go.sh
@@ -2,9 +2,11 @@
 
 if [ -f go.mod ]; then go mod download; fi
 
+rm -rf gotest.log
+
 go test -test.count 1 -test.timeout 30s -coverprofile=./coverage.txt -v ./... | tee gotest.log
 
-if [ "$(tail -n 1 gotest.log | grep -c FAIL)" -gt 0 ]; then exit 1; fi; 
+if [ "$(tail -n 1 gotest.log | grep -c FAIL)" -gt 0 ]; then exit 1; fi;
 
 curl -s https://codecov.io/bash > codecov.sh
 
